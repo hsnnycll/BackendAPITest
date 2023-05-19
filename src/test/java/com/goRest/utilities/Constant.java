6b84c287e673d176dcc95741f05f65822e5aecbd
Hasan YUCEL <hsnnycll@gmail.com>
2023-05-19 11:14:53 +0300

    add new file

---
+++ src/test/java/com/goRest/utilities/Constant.java
@@ -0,0 +1,7 @@
+package com.goRest.utilities;
+
+public class Constant {
+    public static final String BASE_URL = "https://gorest.co.in";
+    public static final String TOKEN = "3e5ced74ea10a5ac21671fa868e26e0a5b8d472752fa66b496dec43aeb5b35b4";
+
+}
